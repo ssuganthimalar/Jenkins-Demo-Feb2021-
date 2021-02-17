d378a25e994cd41e2cc7304fb83c6cfc27eed6cf
ssuganthimalar <ssuganthimalar@gmail.com>
2021-02-17 17:02:09 +0000

    Committing to local repo

---
+++ src/main/java/MavenMain.java
@@ -0,0 +1,6 @@
+public class MavenMain {
+    public void mavenMETHOD(){
+        System.out.println("Maven is cool");
+        System.out.println("Maven is interesting");
+    }
+}
---
+++ src/test/java/MavenTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class MavenTest {
+    @Test
+    public void test1(){
+        System.out.println("Test 1 is running");
+    }
+    @Test
+    public void test2(){
+        System.out.println("Test 2 is running");
+    }
+}
